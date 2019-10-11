407545b1273219e68f23776a69cb760384400028
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-10-11 18:03:20 +0800

    Documentation update

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "object" {
+  description = "Output the full object"
   value = azurerm_monitor_diagnostic_setting.diagnostics
 }
 
 output "name" {
+  description = "Output the object name"
   value = azurerm_monitor_diagnostic_setting.diagnostics.name
 }
 
 output "id" {
+  description = "Output the object ID"
   value = azurerm_monitor_diagnostic_setting.diagnostics.id
 }
